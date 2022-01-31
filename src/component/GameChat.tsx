64395e200fa9feb8e59f6eeb2bf32b398917dfe7
quentinfon <qfon.dev@gmail.com>
2022-01-31 21:43:30 +0100

    Chat debut

--- src/component/GameChat.tsx
+++ src/component/GameChat.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import { Avatar, List, Card, Comment, Form, Input } from "antd";
+import React, { useState } from "react";
 import { GameMessage } from "../types/message";
+
+const { TextArea } = Input;
 
 interface GameChatProps {
     messages: GameMessage[]
@@ -9,8 +12,53 @@ const GameChat = ({
     messages
 }: GameChatProps) => {
 
+    const [currentMsg, setCurrentMessage] = useState<string>("");
+
     return (
         <>
+        
+            <div
+                style={{
+                    height: "700px"
+                }}
+            >
+                <List
+                    dataSource={messages}
+                    style={{
+                        overflow: "scroll",
+                        maxHeight: "100%"
+                    }}
+                    renderItem={(msg: GameMessage, idx: number) => (
+                        <Card 
+                            size="small"
+                        >
+                            <List.Item key={idx}>
+                                <List.Item.Meta
+                                    avatar={<Avatar src={msg.authorImg} />}
+                                    title={<>{msg.author}</>}
+                                    description={msg.message}
+                                />
+                            </List.Item>
+                        </Card>
+                    )}
+                />
+            </div>
+            <div>
+                 <Card>
+                    <Comment
+                        avatar={<Avatar src="https://joeschmoe.io/api/v1/random" alt="Han Solo" />}
+                        content={
+                            <Form.Item>
+                                <TextArea 
+                                    rows={3} 
+                                    onChange={(e: any) => setCurrentMessage(e.target.value)} 
+                                    value={currentMsg} 
+                                />
+                            </Form.Item>
+                        }
+                    />
+                 </Card>
+            </div>
         
         </>
     )
--- src/route/GamePage.tsx
+++ src/route/GamePage.tsx
@@ -1,12 +1,108 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { Layout } from 'antd';
+import { Layout, Row, Col } from 'antd';
+import { GameMessage } from "../types/message";
+import GameChat from "../component/GameChat";
 
 const { Header, Footer, Sider, Content } = Layout;
 
 const GamePage = () => {
 
     const { gameId } = useParams<{gameId: string}>();
+
+    const fakeMsg : GameMessage[] = [
+        {
+            authorImg: 'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png',
+            author: 'Bob',
+            message: 'Salut tout le monde'
+        },
+        {
+            authorImg: 'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png',
+            author: 'Bob',
+            message: 'Salut tout le monde'
+        },
+        {
+            authorImg: 'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png',
+            author: 'Bob',
+            message: 'Salut tout le monde'
+        },
+        {
+            authorImg: 'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png',
+            author: 'Bob',
+            message: 'Salut tout le monde'
+        },
+        {
+            authorImg: 'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png',
+            author: 'Bob',
+            message: 'Salut tout le monde'
+        },
+
+        {
+            authorImg: 'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png',
+            author: 'Bob',
+            message: 'Salut tout le monde'
+        },
+        {
+            authorImg: 'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png',
+            author: 'Bob',
+            message: 'Salut tout le monde'
+        },
+        {
+            authorImg: 'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png',
+            author: 'Bob',
+            message: 'Salut tout le monde'
+        },
+        {
+            authorImg: 'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png',
+            author: 'Bob',
+            message: 'Salut tout le monde'
+        },
+        {
+            authorImg: 'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png',
+            author: 'Bob',
+            message: 'Salut tout le monde'
+        },
+        {
+            authorImg: 'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png',
+            author: 'Bob',
+            message: 'Salut tout le monde'
+        },
+        {
+            authorImg: 'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png',
+            author: 'Bob',
+            message: 'Salut tout le monde'
+        },
+        {
+            authorImg: 'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png',
+            author: 'Bob',
+            message: 'Salut tout le monde'
+        },
+        {
+            authorImg: 'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png',
+            author: 'Bob',
+            message: 'Salut tout le monde'
+        },
+        {
+            authorImg: 'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png',
+            author: 'Bob',
+            message: 'Salut tout le monde'
+        },
+        {
+            authorImg: 'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png',
+            author: 'Bob',
+            message: 'Salut tout le monde'
+        },
+        {
+            authorImg: 'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png',
+            author: 'Bob',
+            message: 'Salut tout le monde'
+        },
+        {
+            authorImg: 'https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png',
+            author: 'Bob',
+            message: 'Salut tout le monde ferfeeeeeeeeeeeeeeeeehb hbjhre bfjhrebfnjhr ebnfhjubn ferhjuf '
+        }
+    ]
 
     return (
         <>
@@ -16,12 +112,20 @@ const GamePage = () => {
             <Layout>
                 <Header>Header</Header>
                 <Layout>
-                    <Content>Content</Content>
-                    <Sider
-                        collapsible
-                    >
-                        Sider
-                    </Sider>
+                    <Content>
+                        <Row>
+                            <Col span={18}>
+
+
+                            </Col>
+
+                            <Col span={6}>
+                                <GameChat messages={fakeMsg} />
+                            </Col>
+                        </Row>
+
+                    </Content>
+                  
                 </Layout>
                 <Footer>Footer</Footer>
             </Layout>
