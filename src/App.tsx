ecb6e7456573f2562b352b5dce16fdb70fa88643
Quentin Fontaine <qfon.dev@gmail.com>
2022-01-31 15:22:44 +0100

    Init react project

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from "react-router-dom";
+import HomePage from './route/HomePage';
+
+const App = () => {
+    return (
+        <Router>
+            
+            <Routes>
+            
+                <Route path="/" element={<HomePage/>}/>
+
+            </Routes>
+
+        </Router>
+    );
+}
+
+export default App;
---
+++ src/route/HomePage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Home from "../component/Home";
+
+const HomePage = () => {
+
+    return (
+        <>
+
+            Welcome at the home page
+
+            <Home/>
+        
+        </>
+    )
+}
+
+export default HomePage;
