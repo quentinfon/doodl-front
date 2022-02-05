05336aef8b0e71351c32f24acad4da7c6a23b9b5
quentinfon <qfon.dev@gmail.com>
2022-02-05 18:41:40 +0100

    choose nickname

--- src/component/Room/PlayerCreation.tsx
+++ src/component/Room/PlayerCreation.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import { Button } from "antd";
+import { Button, Row, Col, Card, Typography, Input } from "antd";
 import { IPlayer } from "../../types/game";
 
+const { Title, Text } = Typography;
 interface PlayerCreationProps {
     createPlayer: (player: IPlayer) => any,
     loadingConnexion: boolean
@@ -21,15 +22,51 @@ const PlayerCreation = ({
     return (
         <>
 
-            Pseudo :
+            <Row 
+                justify="center"
+                style={{marginTop: "5%"}}
+            >
+                <Col lg={22}>
 
-            <Button 
-                onClick={() => createPlayer(player)}
-                disabled={loadingConnexion}
-                loading={loadingConnexion}
-            > 
-                Connexion
-            </Button>
+                    <Card>
+
+                        <Title level={3}>Joining game</Title>
+
+                        <Row>
+
+                            <Col sm={24} md={12} lg={6}>
+                                <Text>Nickname</Text>
+                                <Input.Group
+                                    style={{ width: "100%" }}
+                                >
+                                    <Input
+                                        value={player.name}
+                                        onChange={(e) => setPlayer({...player, name: e.target.value})}
+                                    />
+                                </Input.Group>
+                            </Col>
+
+                        </Row>
+
+                        <Row
+                            style={{
+                                marginTop: "15px"
+                            }}
+                        >
+                            <Button 
+                                onClick={() => createPlayer(player)}
+                                disabled={loadingConnexion}
+                                loading={loadingConnexion}
+                            > 
+                                Join
+                            </Button>
+                        </Row>
+
+                    </Card>
+                
+                </Col>
+            </Row>
+           
         
         </>
     )
