b161d3d975d97fe13e28bb3e3c16275dc03de388
Quentin Fontaine <qfon.dev@gmail.com>
2022-02-18 09:49:26 +0100

    Debut export components + lobby

--- src/component/Room/Lobby/RoomLobby.tsx
+++ src/component/Room/Lobby/RoomLobby.tsx
@@ -1,5 +1,7 @@
+import {Col, Row} from "antd";
 import React from "react";
-import {IDataInfoResponse} from "../../types/SocketModel";
+import {IDataInfoResponse} from "../../../types/SocketModel";
+import LobbyPlayerList from "./LobbyPlayerList";
 
 interface RoomLobbyProps {
     gameData: IDataInfoResponse
@@ -11,6 +13,18 @@ const RoomLobby = ({
 
     return (
         <>
+            <Row>
+                <Col sm={24} lg={12}>
+
+                </Col>
+
+                <Col sm={24} lg={12}>
+                    <LobbyPlayerList
+                        adminPlayerId={""}
+                        players={gameData.playerList}
+                    />
+                </Col>
+            </Row>
 
         </>
     )
