eef2ae531e1d6134dc98b90d5630f8bc8eb32ec0
Quentin Fontaine <qfon.dev@gmail.com>
2022-02-18 09:49:09 +0100

    Debut export components + lobby

--- src/component/Room/Lobby/RoomLobby.tsx
+++ src/component/Room/Lobby/RoomLobby.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import {IDataInfoResponse} from "../../types/SocketModel";
 
 interface RoomLobbyProps {
-
+    gameData: IDataInfoResponse
 }
 
 const RoomLobby = ({
-
+                       gameData
                    }: RoomLobbyProps) => {
 
     return (
