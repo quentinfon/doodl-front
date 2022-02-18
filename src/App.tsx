01c2ca4052cf0f73253201c835529ec15df817fb
PRTH9465 <thomas.fillion@ecole.ensicaen.fr>
2022-02-18 14:42:41 +0100

    feature : autorefresh every 5sec admin stats + admin get out of header

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ const App = () => {
                     <Routes>
                         <Route path="/play/:gameId" element={<GamePage/>}/>
                         <Route path="/" element={<HomePage/>}/>
-                        <Route path="/admin" element={<AdminPage/>}/>
                         <Route
                             path="*"
                             element={<Navigate to="/"/>}
