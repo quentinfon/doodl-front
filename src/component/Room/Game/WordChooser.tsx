45c782124cdbcab4562124346a45b0d7b66c786b
Quentin Fontaine <qfon.dev@gmail.com>
2022-02-23 22:37:23 +0100

    Debut overlay

---
+++ src/component/Room/Game/WordChooser.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {Button, Col, Row, Typography} from "antd";
+
+const {Title} = Typography;
+
+interface WordChooserProps {
+    words: string[],
+    chooseWord: (word: string) => any
+}
+
+const WordChooser = ({
+                         words,
+                         chooseWord
+                     }: WordChooserProps) => {
+
+    console.log(words);
+
+    return (
+        <>
+            <Row justify="center">
+                <Title
+                    level={3}
+                    style={{
+                        color: "#fff"
+                    }}
+                >
+                    Choose a word
+                </Title>
+            </Row>
+
+
+            <Row
+                justify={"center"}
+                gutter={[10, 10]}
+            >
+                {["Test", "Billy boy", "Bob"].map((word: string) => {
+
+                    return (
+                        <Col xs={24}>
+                            <Button
+                                type="dashed"
+                                onClick={() => chooseWord(word)}
+                                style={{
+                                    width: "100%"
+                                }}
+                            >
+                                {word}
+                            </Button>
+                        </Col>
+                    )
+                })}
+            </Row>
+
+        </>
+    )
+}
+
+export default WordChooser;
