3ac46e3f6526eaadb442f422c6d67757a54eb3fb
Thomas Fillion <thomas.fillion@orange.com>
2022-02-25 12:07:20 +0100

    complete sound enum

--- src/component/Room/Component/Sound.tsx
+++ src/component/Room/Component/Sound.tsx
@@ -1,8 +1,8 @@
 export enum Sound {
-    WIN_ROUND = "",
-    WIN_GAME = "",
-    WORD_GUESSED = "",
+    END_START = "/roundStart.mp3",
+    END_ROUND = "/roundEnd.mp3",
+    WORD_GUESSED = "/guessed.mp3",
     YOUR_TURN_TO_DRAW = "",
-    GAME_JOIN = "",
-    GAME_LEAVE = ""
+    GAME_JOIN = "/join.mp3",
+    GAME_LEAVE = "/leave.mp3"
 }
