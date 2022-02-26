d292e9d5577c9d421b3b1d0f7985bdaa323f2617
Thomas Fillion <thomas.fillion@orange.com>
2022-02-26 22:12:06 +0100

    fix : precedent commit - redo file paths

--- src/component/Room/Component/Sound.tsx
+++ src/component/Room/Component/Sound.tsx
@@ -1,8 +1,8 @@
 export enum Sound {
-    ROUND_START = "/roundStart.mp3",
-    ROUND_END = "/roundEnd.mp3",
-    WORD_GUESSED = "/guessed.mp3",
+    ROUND_START = "/Song/roundStart.mp3",
+    ROUND_END = "/Song/roundEnd.mp3",
+    WORD_GUESSED = "/Song/guessed.mp3",
     YOUR_TURN_TO_DRAW = "",
-    GAME_JOIN = "/join.mp3",
-    GAME_LEAVE = "/leave.mp3"
+    GAME_JOIN = "/Song/join.mp3",
+    GAME_LEAVE = "/Song/leave.mp3"
 }
